ba49be8cca7c41eb2c991333d6991b74e5a9b38a
Gary Stafford <garystafford@rochester.rr.com>
2017-01-25 08:48:20 -0500

    Changing build artifact location

--- deploy_travisci.sh
+++ deploy_travisci.sh
@@ -16,4 +16,4 @@ git config user.email "${COMMIT_AUTHOR_EMAIL}"
 
 git add *.jar
 git commit -m "Deploy Travis CI Build #${TRAVIS_BUILD_NUMBER} artifacts to GitHub"
-git push --force --quiet "https://${GH_TOKEN}@${GH_ARTIFACT_REPO}" master:master
+git push --force --quiet "https://${GH_TOKEN}@${GH_ARTIFACT_REPO}" master:build-artifacts
