bb843e7bccff9f30971f3baeb03d2180ecae76b0
Gary Stafford <garystafford@rochester.rr.com>
2017-01-25 20:52:30 -0500

    Docker build now happens on Travis CI not Docker Hub

--- deploy_travisci.sh
+++ deploy_travisci.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-# Deploys JAR build artifact to GitHub (acts as pseudo binary repository)
-# (3) encrypted environment variables in .travis.yml file used here
+# Builds and deploys JAR build artifact to GitHub (acts as binary repository)
+# Builds immutable Docker Image, deploying the JAR, above.
+
+# Uses encrypted environment variables in .travis.yml file used here
 # travis encrypt GH_TOKEN=<your_token_hash> --add
 # travis encrypt COMMIT_AUTHOR_EMAIL=<your_email_here> --add
 # travis encrypt GH_ARTIFACT_REPO=github.com/<your_repo_path>.git --add
+# travis encrypt DOCKER_USERNAME=<your_username> --add
+# travis encrypt DOCKER_PASSWORD=<your_password> --add
 
 #set -x
 
-cd build/libs
-
+pushd build/libs
 git init
 git config user.name "travis-ci"
 git config user.email "${COMMIT_AUTHOR_EMAIL}"
@@ -18,6 +21,7 @@ git add *.jar
 git commit -m "Deploy Travis CI Build #${TRAVIS_BUILD_NUMBER} artifacts to GitHub"
 git push --force --quiet "https://${GH_TOKEN}@${GH_ARTIFACT_REPO}" master:build-artifacts
 
+popd
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 
 docker build -t garystafford/voter-service:latest .
