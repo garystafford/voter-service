4d670a3d4bb7d9b6d3ec88f21744b3708bb6213a
Gary A. Stafford <garystafford@rochester.rr.com>
2017-04-17 23:24:19 -0400

    Create scripts directory

---
+++ scripts/stack_deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Deploys the voter_stack to the Docker swarm cluster
+
+set -e
+
+docker-machine env manager1
+eval $(docker-machine env manager1)
+
+export CONSUL_SERVER=$(docker-machine ip $(docker node ls | grep Leader | awk '{print $3}'))
+export WIDGET_PROFILE=docker-local
+
+docker stack deploy --compose-file=docker-compose.yml voter_stack
+
+echo "Letting services start-up..."
+sleep 5
+
+docker stack ls
+docker stack ps widget_stack --no-trunc
+docker service ls
+
+echo "Script completed..."
---
+++ scripts/stack_validate.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Checks if Voter Service is up
+
+HOST_IP=$(docker-machine ip worker2)
+ATTEMPTS=16
+SLEEPTIME=15
+
+until curl -s --head "${HOST_IP}:8099";
+do
+  echo "Attempt ${ATTEMPTS}..."
+
+  if [ $ATTEMPTS -eq 0 ]
+  then
+    break
+  fi
+
+  echo "Waiting ${SLEEPTIME} more seconds to see if things are working..."
+
+  sleep $SLEEPTIME
+  let ATTEMPTS-=1
+done
+
+curl "${HOST_IP}:8099/health"
