f1bdaa668d9aebffbe1bced6576179843237c6f0
Gary A. Stafford <garystafford@rochester.rr.com>
2017-04-18 23:03:50 -0400

    Create uber runner script

---
+++ scripts/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Runs all project scripts...
+
+set -e
+
+echo "Deploying Stack: Widget, MongoDB..."
+sh ./stack_deploy.sh
+
+echo "Waiting for Stack to be ready..."
+sh ./stack_validate.sh
