46e16eacf4e51c350c87cf0b4c260f3e7b854393
Gary A. Stafford <garystafford@rochester.rr.com>
2017-04-20 21:36:51 -0400

    Adding election field, fixing tests

--- src/main/java/com/example/voter/Application.java
+++ src/main/java/com/example/voter/Application.java
@@ -1,9 +1,7 @@
 package com.example.voter;
 
-import org.springframework.amqp.core.DirectExchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
