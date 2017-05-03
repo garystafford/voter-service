400db62c801b25c8eadc3a61f85344777b1d4ca0
Gary A. Stafford <garystafford@rochester.rr.com>
2017-05-03 10:59:09 -0400

    Refactor scripts for post

---
+++ scripts-services/demostrate_api_ipc.sh
@@ -0,0 +1,23 @@
+
+#!/bin/sh
+
+# Demostrate API calls for REST HTTP IPC and RPC IPC via API Gateway
+# Requires HTTPie
+# Requires all services are running
+
+set -e
+
+echo "\nUsing REST HTTP IPC...\n"
+http http://localhost:8080/candidate/simulation && sleep 2
+http http://localhost:8080/candidate/candidates/summary/election/2016%20Presidential%20Election && sleep 2
+
+http http://localhost:8080/voter/simulation/election/2016%20Presidential%20Election && sleep 2
+http http://localhost:8080/voter/results && sleep 4
+http http://localhost:8080/voter/winners && sleep 2
+
+echo "\nUsing Message-based RPC IPC...\n"
+http http://localhost:8080/voter/simulation/rpc/election/2016%20Presidential%20Election && sleep 2
+http http://localhost:8080/voter/results && sleep 2
+http http://localhost:8080/voter/winners && sleep 2
+
+echo "\nScript completed...\n"
