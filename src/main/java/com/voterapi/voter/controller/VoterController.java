c407878a2fe1eaf7d03a76276c284fd20f1c474a
Gary Stafford <gary.stafford@accenture.com>
2017-11-27 10:36:03 -0500

    Updating from rabbitmq branch

--- src/main/java/com/voterapi/voter/controller/VoterController.java
+++ src/main/java/com/voterapi/voter/controller/VoterController.java
@@ -33,13 +33,9 @@ import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 public class VoterController {
 
     private MongoTemplate mongoTemplate;
-
     private VoterRepository voterRepository;
-
     private CandidateRepository candidateRepository;
-
     private VoterSeedDataService voterSeedDataService;
-
     private CandidateListService candidateListService;
 
     @Autowired
@@ -55,7 +51,7 @@ public class VoterController {
         this.candidateListService = candidateListService;
     }
 
-    @RequestMapping(value = "/candidates/election/{election}", method = RequestMethod.GET)
+    @RequestMapping(value = "/candidates/db/{election}", method = RequestMethod.GET)
     public ResponseEntity<Map<String, List<CandidateVoterView>>> getCandidatesDb(@PathVariable("election") String election) {
         List<CandidateVoterView> results = candidateListService.getCandidatesQueueDb(election);
         return new ResponseEntity<>(Collections.singletonMap("candidates", results), HttpStatus.OK);
@@ -75,11 +71,12 @@ public class VoterController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @RequestMapping(value = "/results", method = RequestMethod.GET)
-    public ResponseEntity<Map<String, List<VoteCount>>> getResults() {
+    @RequestMapping(value = "/results/{election}", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, List<VoteCount>>> getResults(@PathVariable("election") String election) {
 
         Aggregation aggregation = Aggregation.newAggregation(
-                Aggregation.group("candidate").count().as("votes"),
+                match(Criteria.where("election").is(election)),
+                group("candidate").count().as("votes"),
                 project("votes").and("candidate").previousOperation(),
                 sort(Sort.Direction.DESC, "votes")
         );
@@ -90,11 +87,12 @@ public class VoterController {
         return new ResponseEntity<>(Collections.singletonMap("results", results), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/results/votes", method = RequestMethod.GET)
-    public ResponseEntity<VoteCountWinner> getTotalVotes() {
+    @RequestMapping(value = "/results/{election}/votes", method = RequestMethod.GET)
+    public ResponseEntity<VoteCountWinner> getTotalVotes(@PathVariable("election") String election) {
 
         Query query = new Query();
         query.addCriteria(Criteria.where("candidate").exists(true));
+        query.addCriteria(Criteria.where("election").is(election));
 
         Long groupResults =
                 mongoTemplate.count(query, Vote.class);
@@ -103,12 +101,13 @@ public class VoterController {
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
-    @RequestMapping(value = "/winners", method = RequestMethod.GET)
-    public ResponseEntity<Map<String, List<VoteCount>>> getWinners() {
+    @RequestMapping(value = "/winners/{election}", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, List<VoteCount>>> getWinners(@PathVariable("election") String election) {
 
         Aggregation aggregation = Aggregation.newAggregation(
-                Aggregation.group("candidate").count().as("votes"),
-                match(Criteria.where("votes").is(getWinnersVotesInt())),
+                match(Criteria.where("election").is(election)),
+                group("candidate").count().as("votes"),
+                match(Criteria.where("votes").is(getWinnersVotesInt(election))),
                 project("votes").and("candidate").previousOperation(),
                 sort(Sort.Direction.ASC, "candidate")
         );
@@ -119,18 +118,19 @@ public class VoterController {
         return new ResponseEntity<>(Collections.singletonMap("results", results), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/winners/votes", method = RequestMethod.GET)
-    public ResponseEntity<VoteCountWinner> getWinnersVotes() {
+    @RequestMapping(value = "/winners/{election}/votes", method = RequestMethod.GET)
+    public ResponseEntity<VoteCountWinner> getWinnersVotes(@PathVariable("election") String election) {
 
-        VoteCountWinner result = new VoteCountWinner(getWinnersVotesInt());
+        VoteCountWinner result = new VoteCountWinner(getWinnersVotesInt(election));
 
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
-    private int getWinnersVotesInt() {
+    private int getWinnersVotesInt(String election) {
 
         Aggregation aggregation = Aggregation.newAggregation(
-                Aggregation.group("candidate").count().as("votes"),
+                match(Criteria.where("election").is(election)),
+                group("candidate").count().as("votes"),
                 project("votes"),
                 sort(Sort.Direction.DESC, "votes"),
                 limit(1)
@@ -145,7 +145,7 @@ public class VoterController {
         return groupResults.getMappedResults().get(0).getVotes();
     }
 
-    @RequestMapping(value = "/simulation/election/{election}", method = RequestMethod.GET)
+    @RequestMapping(value = "/simulation/{election}", method = RequestMethod.GET)
     public ResponseEntity<Map<String, String>> getSimulationDb(@PathVariable("election") String election) {
 
         voterRepository.deleteAll();
